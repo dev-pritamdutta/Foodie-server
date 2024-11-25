bc4574737f8b62354a491b87b6938477a5323ba9
pritam dutta <webdevpritam7498@gmail.com>
2024-11-25 22:00:55 +0600

    updated

--- index.js
+++ index.js
@@ -1,32 +1,17 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const helmet = require("helmet"); // Helmet for setting security headers
+const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 3000; // Make sure "process.env.PORT" is capitalized for render
+const port = process.env.port || 3000;
 require("dotenv").config();
 
-const app = express();
-
-// Middleware
+// middleware
 app.use(cors());
 app.use(express.json());
 
-// Set up Helmet with CSP headers
-app.use(
-  helmet.contentSecurityPolicy({
-    useDefaults: true,
-    directives: {
-      defaultSrc: ["'self'"],
-      fontSrc: ["'self'", "https://foodie-server-7mst.onrender.com"],
-      imgSrc: ["'self'", "data:"], // Allows loading inline base64 images
-      scriptSrc: ["'self'", "'unsafe-inline'"],
-      styleSrc: ["'self'", "'unsafe-inline'"], // Allows inline styles
-    },
-  })
-);
 
-// MongoDB connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@foodi.sk0tb.mongodb.net/?retryWrites=true&w=majority&appName=foodi`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -38,35 +23,33 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    // Database and collections
+    //database and collection
     const menuCollection = client.db("demo-foodi-client").collection("menus");
-    const cartCollection = client
-      .db("demo-foodi-client")
-      .collection("cartItems");
+    const cartCollection = client.db("demo-foodi-client").collection("cartItems");
 
-    // Endpoint to get all menu items
-    app.get("/menu", async (req, res) => {
+    //all menu items operation
+    app.get("/menu", async(req, res) =>{
       const result = await menuCollection.find().toArray();
       res.send(result);
-    });
+    })
 
-    // Ping the database to confirm the connection
+
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Keep the client open for further operations
+    // Ensures that the client will close when you finish/error
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-// Root endpoint
 app.get("/", (req, res) => {
   res.send("Hello developer");
 });
 
-// Start the server
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
