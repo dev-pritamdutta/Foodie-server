4ded2dbf4be8311d9b331ae0d90c291047731ea7
pritam dutta <webdevpritam7498@gmail.com>
2024-12-13 15:21:18 +0600

    cart api created

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const cors = require("cors");
 const port = process.env.port || 3000;
@@ -8,7 +8,6 @@ require("dotenv").config();
 // middleware
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@foodi.sk0tb.mongodb.net/?retryWrites=true&w=majority&appName=foodi`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,15 +24,66 @@ async function run() {
     await client.connect();
     //database and collection
     const menuCollection = client.db("demo-foodi-client").collection("menus");
-    const cartCollection = client.db("demo-foodi-client").collection("cartItems");
+    const cartCollection = client
+      .db("demo-foodi-client")
+      .collection("cartItems");
 
     //all menu items operation
-    app.get("/menu", async(req, res) =>{
+    app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
-    })
+    });
 
+    //all cart items operation
 
+    //posting cart to db
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+
+    //get carts using email
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const filter = { email: email };
+      const result = await cartCollection.find(filter).toArray();
+      res.send(result);
+    });
+
+    //get specific carts
+    app.get("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await cartCollection.findOne(filter);
+      res.send(result);
+    });
+
+    //delete items from cart
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(filter);
+      res.send(result);
+    });
+
+    //update cart quantity
+    app.put("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const { quantity } = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+
+      const updateDoc = {
+        $set: {
+          quantity: parseInt(quantity, 10),
+        },
+      };
+
+      const result = await cartCollection.updateOne(filter, updateDoc, options);
+
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
